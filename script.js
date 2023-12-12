4b147ecbdd4e9543fd5daa620c19c39d40bb6c8d
Tarık <tariknxopeiyik@gmail.com>
2023-12-12 14:46:08 +0300

    12.12 update1.5

--- script.js
+++ script.js
@@ -116,3 +116,57 @@ topButton.addEventListener("click", function () {
     topButton.style.opacity = "0";
   }, 300);
 });
+
+// This function is called when the custom dropdown is clicked
+function toggleCustomSelect(wrapper) {
+  // This line toggles the visibility of the dropdown options
+  wrapper.querySelector(".custom-select-options").classList.toggle("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Toggles the custom select options
+  document
+    .querySelectorAll(".custom-select-visible")
+    .forEach(function (select) {
+      select.addEventListener("click", function () {
+        this.nextElementSibling.classList.toggle("hidden"); // This assumes that the ul element is right after the div
+      });
+    });
+
+  // Updates the placeholder and the actual select value when an option is clicked
+  document
+    .querySelectorAll(".custom-select-options li")
+    .forEach(function (option) {
+      option.addEventListener("click", function () {
+        const wrapper = this.closest(".custom-select-wrapper");
+        const text = this.textContent;
+        const value = this.getAttribute("data-value");
+        const placeholder = wrapper.querySelector(".custom-select-placeholder");
+        const nativeSelect = wrapper.querySelector(".custom-select");
+
+        // Update the placeholder text
+        placeholder.textContent = text;
+
+        // Update the value of the native select element
+        nativeSelect.value = value;
+
+        // Hide the custom options again
+        wrapper.querySelector(".custom-select-options").classList.add("hidden");
+
+        // Optional: Trigger any change event listeners attached to the native select
+        const event = new Event("change", { bubbles: true });
+        nativeSelect.dispatchEvent(event);
+      });
+    });
+
+  // Close the custom select options if clicking outside of the select
+  window.addEventListener("click", function (event) {
+    if (!event.target.matches(".custom-select-visible")) {
+      document
+        .querySelectorAll(".custom-select-options")
+        .forEach(function (optionsList) {
+          optionsList.classList.add("hidden");
+        });
+    }
+  });
+});
